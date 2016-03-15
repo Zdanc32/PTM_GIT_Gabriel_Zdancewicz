e79979059dfab3f54492268e292538ebe5681ca6
admin <admin@admin.admin>
2016-03-15 08:58:02 +0100

    Dobra
    Dodaje

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTM_GIT_Gabriel_Zdancewicz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git!");
+
+        }
+    }
+}
